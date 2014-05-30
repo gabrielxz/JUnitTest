62dd004b73ddb33d09276ba984084aac0b3151f0
Gabriel Beal <gabrielbeal@gmail.com>
2014-05-30 14:16:12 -0400

    2nd commit

--- src/test/java/test/java/TestLocalRun.java
+++ src/test/java/test/java/TestLocalRun.java
@@ -1,89 +1,58 @@
+
 package test.java;
-
-										 
-
+ 
 import static junit.framework.Assert.assertTrue;
-
 import java.io.IOException;
-
 import org.junit.After;
-
 import org.junit.Before;
-
 import org.junit.Test;
-
 import org.openqa.selenium.By;
-
 import org.openqa.selenium.WebDriver;
-
 import org.openqa.selenium.WebElement;
-
 import org.openqa.selenium.firefox.FirefoxDriver;
-
 import org.openqa.selenium.support.ui.ExpectedCondition;
-
 import org.openqa.selenium.support.ui.WebDriverWait;
-
  
-
  
-
 public class TestLocalRun {
-
      private WebDriver driver;
-
      private WebDriverWait wait;
-
     
-
     
-
       @Before
-
-      public void setUp() { 
-
-        driver = new FirefoxDriver(); 
-
+      public void setUp() {
+        driver = new FirefoxDriver();
         wait = new WebDriverWait(driver, 10);
-
-        driver.get("http://www.google.com"); 
-
-      } 
-
+        driver.get("http://www.google.com");
+      }
       @After
-
-      public void tearDown() throws IOException { 
-
-        driver.quit(); 
-
+      public void tearDown() throws IOException {
+        driver.quit();
       } 
-
  
-
       @Test
-
-      public void pageTitleAfterSearchShouldBeginWithDrupal() throws IOException { 
-
-        WebElement searchField = driver.findElement(By.name("q")); 
-
-        searchField.sendKeys("Drupal!"); 
-
-        searchField.submit(); 
-
-        assertTrue("The page title should start with the search string after the search.", 
-
-            wait.until(new ExpectedCondition<Boolean>() { 
-
-              public Boolean apply(WebDriver d) { 
-
-                return d.getTitle().toLowerCase().startsWith("drupal!"); 
-
-              } 
-
-            })); 
-
-      } 
-
+      public void pageTitleAfterSearchShouldBeginWithDrupal() throws IOException {
+        WebElement searchField = driver.findElement(By.name("q"));
+        searchField.sendKeys("Drupal!");
+        searchField.submit();
+        assertTrue("The page title should start with the search string after the search.",
+            wait.until(new ExpectedCondition<Boolean>() {
+              public Boolean apply(WebDriver d) {
+                return d.getTitle().toLowerCase().startsWith("drupal!");
+              }
+            }));
+      }
+      
+      @Test
+      public void pageTitleAfterSearchShouldBeginWithDrupal2() throws IOException 
+      {
+        WebElement searchField = driver.findElement(By.name("q"));
+        searchField.sendKeys("Drupal!");
+        searchField.submit();
+        assertTrue("The page title should start with the search string after the search.",
+                driver.getTitle().toLowerCase().startsWith("2drupal!"));
+      }
+            
+      
     
-
 }
